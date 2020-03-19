1676a73c01e33cb6e7dedc9dc0419cd1fdc68950
Ender Erol <ender.em@gmail.com>
2020-03-19 15:36:30 +0100

    added map result method

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -15,16 +15,8 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         var tempScore = 0
         if (isDraw()) {
             return mapDrawMessage()
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
-            if (minusResult == 1)
-                score = "Advantage player1"
-            else if (minusResult == -1)
-                score = "Advantage player2"
-            else if (minusResult >= 2)
-                score = "Win for player1"
-            else
-                score = "Win for player2"
+        } else if (isGameFinished()) {
+            return mapMatchResult(score)
         } else {
             for (i in 1..2) {
                 if (i == 1)
@@ -43,6 +35,22 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         }
         return score
     }
+
+    private fun mapMatchResult(score: String): String {
+        var score1 = score
+        val minusResult = m_score1 - m_score2
+        if (minusResult == 1)
+            score1 = "Advantage player1"
+        else if (minusResult == -1)
+            score1 = "Advantage player2"
+        else if (minusResult >= 2)
+            score1 = "Win for player1"
+        else
+            score1 = "Win for player2"
+        return score1
+    }
+
+    private fun isGameFinished() = m_score1 >= 4 || m_score2 >= 4
 
     private fun isDraw() = m_score1 == m_score2
 
